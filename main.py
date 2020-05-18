74eb78d68b479c070dd961714dbb88076256c218
Jacob Schwartz <jtschwartz98@gmail.com>
2020-05-18 12:26:43 -0400

    Smarter approach: GDrive on the back burner. Further development on configuration and detecting the requested files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from settings import *
 from file_management import *
+from os import walk as traverse
+import re
+from settings import *
 from threader import Threader
 
 
@@ -18,11 +20,42 @@ def run_threads():
 def main():
 	settings.init()
 	
-	if settings.drive is None:
-		print("Err")
+	# Needed for GDrive implementation
+	# if settings.drive is None:
+	# 	print("Err")
+
+	# print(all_files())
+	directory = "C:\\Users\\jtsch\\Workspace"
+	for dirName, subdirList, fileList in traverse(directory):
+		if test_excluded_paths(dirName):
+			continue
+		
+		print('Found directory: %s' % dirName)
+		for file in fileList:
+			if test_excluded_files(file):
+				continue
+				
+			print('\t%s' % file)
 
 
-# print(all_files())
+def test_excluded_paths(path):
+	for exclude in settings.config["directoryExclusions"]:
+		if re.search(exclude, path):
+			return True
+	
+	return False
+
+
+def test_excluded_files(path):
+	for exclude in settings.config["fileTypeExclusions"]:
+		if path.endswith(exclude):
+			return True
+	
+	for exclude in settings.config["fileNameExclusions"]:
+		if re.search(exclude, path):
+			return True
+	
+	return False
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -11,17 +11,15 @@ try:
 except ImportError:
 	from yaml import Loader, Dumper
 
-# output = dump(data, Dumper=Dumper)
-
 gauth = drive = config = None
 
 
 def init():
 	global gauth, drive, config
 	
-	gauth = GoogleAuth()
-	gauth.LocalWebserverAuth()
-	drive = GoogleDrive(gauth)
+	# gauth = GoogleAuth()
+	# gauth.LocalWebserverAuth()
+	# drive = GoogleDrive(gauth)
 	
 	if not exists("config.yaml"):
 		shutil.copyfile("default_config.yaml", "config.yaml")
@@ -29,7 +27,33 @@ def init():
 	load_config(open("config.yaml"))
 
 
+def add_directory(path):
+	global config
+	
+	# Do not include a new path if it is already included within a currently listed directory
+	# Otherwise, remove any directories contained by the new path
+	for entry in config.directories:
+		# A directory can only possibly be contained by an entry if the entry contains less subdirectories
+		if entry.count('/') < path.count('/'):
+			if path.contains(entry):
+				return
+		else:
+			# If an entry is already listed, and is contained within the new path, no higher directory can be listed already
+			# Because of this logic, we can use the same loop to remove lower directories contained by the new directory
+			if entry.contains(path):
+				config.directories.remove(entry)
+				
+	config.directories.append(path)
+	
+	save_config()
+	
+
 def load_config(config_file):
 	global config
 	
 	config = load(config_file, Loader=Loader)
+
+
+def save_config():
+	with open("config.yaml", 'w') as file:
+		dump(config, file)
